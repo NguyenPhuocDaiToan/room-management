012d127e92e96d19874c10b010988b3603ff109d
DAITOAN <daitoan2000@gmail.com>
2023-04-14 13:11:03 +0700

    update connect database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,29 @@
+import * as mongoosePaginate from 'mongoose-paginate-v2';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+
+mongoosePaginate.paginate.options = {
+  lean: true,
+  limit: 20,
+  page: 1,
+};
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://daitoan2000:e3dE0y7hOyJNIvrt@daitoan.uamxmsp.mongodb.net/room-management?retryWrites=true&w=majority',
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        connectionFactory: (connection) => {
+          connection.plugin(mongoosePaginate);
+          return connection;
+        },
+      },
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
